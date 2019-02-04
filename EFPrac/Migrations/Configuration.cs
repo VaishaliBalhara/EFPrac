9245d107fc9296328e527b44d8deea955bf80f4d
Vaishali <vaishali_leo@rediffmail.com>
2019-02-04 06:16:34 -0500

    pract1

---
+++ EFPrac/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace EFPrac.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<EFPrac.SchoolContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(EFPrac.SchoolContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
--- EFPrac/Program.cs
+++ EFPrac/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,4 +14,32 @@ namespace EFPrac
         {
         }
     }
+
+    public class SchoolContext:DbContext
+    {
+        public SchoolContext():base(@"Data Source =.\HUMBERBRIDGING; Initial Catalog = SCHOOL_DB; Integrated Security = True;")
+        {
+
+        }
+
+        DbSet<Student> Students { get; set; }
+        DbSet<Teacher> Teachers { get; set; }
+
+    }
+    public class Student
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Teacher> Teachers { get; set; }
+
+    }
+
+    public class Teacher
+    {
+        [Key]
+        public Guid SomeIdentifier { get; set; }
+        public string Name { get; set; }
+        public ICollection<Student> Students { get; set; }
+    }
+
 }
